350f17fd7b9dbdb92bd0bb4ace9276408805da19
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-26 09:49:23 +0300

    Added UpdateProduct command

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -3,12 +3,12 @@ namespace Catalog.API.Products.GetProductByCategory;
 
 public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
 public record GetProductByCategoryResult(IEnumerable<Product> Products);
-internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) 
+internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) 
     : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
     public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
 
         var products = await session.Query<Product>()
             .Where(x => x.Category.Contains(query.Category))
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Reflection;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+
+public record UpdateProductCommand(Guid Id, string Name, ICollection<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool isSuccess);
+public class UpdateProductCommandHandler
+    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductCommand.Handle called with {@Command}", command);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        UpdateProperties(product, command);
+
+        session.Update(product);
+        await session.SaveChangesAsync();
+
+        return new UpdateProductResult(true);
+    }
+
+    private void UpdateProperties(object target, object source)
+    {
+        Type targetType = target.GetType();
+        Type sourceType = source.GetType();
+
+        PropertyInfo[] sourceProperties = sourceType.GetProperties();
+
+        foreach (PropertyInfo sourceProperty in sourceProperties)
+        {
+            PropertyInfo targetProperty = targetType.GetProperty(sourceProperty.Name);
+            if (targetProperty != null && targetProperty.CanWrite)
+            {
+                object value = sourceProperty.GetValue(source);
+                targetProperty.SetValue(target, value);
+            }
+        }
+    }
+}
