e89453585b5396b250aca223e50a95784dfb9ebe
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-31 15:54:34 +0300

    Added custom DomainException

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -8,7 +8,7 @@ public class CustomerId
         ArgumentNullException.ThrowIfNull(value);
         if(value == Guid.Empty)
         {
-            throw new Exception("CustomerId cannot be empty");
+            throw new DomainException("CustomerId cannot be empty");
         }
 
         return new CustomerId(value);
