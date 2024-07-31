599a578307ba8324179ac0cb532ed97cf33e6af8
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-31 15:51:23 +0300

    Added Of method for create instance of CustomerId

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -1,6 +1,16 @@
 ﻿namespace Ordering.Domain.ValueObjects;
-
 public class CustomerId
 {
     public Guid Value { get; }
+    private CustomerId(Guid value) => Value = value;
+    public static CustomerId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if(value == Guid.Empty)
+        {
+            throw new Exception("CustomerId cannot be empty");
+        }
+
+        return new CustomerId(value);
+    }
 }
