fedf0aeca01140b8c000840ee00641def108bdba
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-01 10:01:01 +0300

    Added entity configurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id)
+            .HasConversion(
+            customerId => customerId.Value, // dbContext'e yazarken ne olarak yazacağız
+            dbId => CustomerId.Of(dbId));  // dbContext'ten alırken ne olarak alacağız
+
+        builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+        builder.Property(c => c.Email).HasMaxLength(255);
+        builder.HasIndex(c => c.Email).IsUnique(); // Email, bir index olarak görev yapacak ve benzersiz olacak
+    }
+}
