b7f80c844b5bee35d457cd79c3f43408adb76b86
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-06 09:58:56 +0300

    Added GetOrdersByName query and handler Added OrderExtensions for converting object to dto

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.CQRS.Requests;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public record GetOrdersByNameQuery(string Name)
+    : IQuery<GetOrdersByNameResult>;
+
+public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
